b8b225aa8211ae22ebee1d4b7fd8357852301194
Ketan Agrawal <agrawalk@stanford.edu>
2020-09-29 19:26:45 -0400

    music on file load works

--- org-spotify.el
+++ org-spotify.el
@@ -20,15 +20,49 @@
 ;;
 ;;; Code:
 
-(defun org-spotify-play (href)
+;;; Customizable variables
+(defgroup org-spotify nil
+  "Store org-mode notes in a Neo4j graph database."
+  :group 'org
+  :prefix "org-spotify-"
+  :link '(url-link :tag "Github" "https://github.com/ketan0/org-spotify"))
+
+(defun org-spotify-play-href (href)
   "Plays HREF on Spotify, using the Alfred Spotify Mini Player."
   (shell-command (format "osascript -e 'tell application id \"%s\" to run trigger \"play\" in workflow \"%s\" with argument \"%s\"'"
                          "com.runningwithcrayons.Alfred"
                          "com.vdesabou.spotify.mini.player"
                          href)))
 
+(defun org-spotify-play-pause ()
+  (shell-command (format "osascript -e 'tell application \"%s\" to %s'"
+                         "Spotify"
+                         "playpause")))
+
 (defvar sample-href "spotify:artist:4r63FhuTkUYltbVAg5TQnk")
-(org-spotify-play sample-href)
+(org-spotify-play-href sample-href)
+
+(defun org-spotify--maybe-play-file-href ()
+  ;; TODO: compatibility if org-collect-keywords doesn't exist
+  (when (eq major-mode 'org-mode)
+             (-some->> (org-collect-keywords '("SPOTIFY_HREF"))
+                  (car)
+                  (cadr)
+                  (org-spotify-play-href)
+                  )))
+
+(define-minor-mode org-spotify-mode
+  "Test."
+  :lighter " Org-spotify"
+  :keymap (let ((map (make-sparse-keymap)))
+            map)
+  :group 'org-spotify
+
+  (if org-spotify-mode
+      (add-hook 'find-file-hook 'org-spotify--maybe-play-file-href)
+    (remove-hook 'find-file-hook 'org-spotify--maybe-play-file-href))
+
+  )
 
 ;;
 ;; (url-retrieve-synchronously )
