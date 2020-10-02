ad255ffd9c04b13ed67557cc69577cb842c75374
Ketan Agrawal <agrawalk@stanford.edu>
2020-10-02 18:52:00 -0400

    can get and set the file's spotify href

--- org-spotify.el
+++ org-spotify.el
@@ -19,14 +19,18 @@
 ;;  Currently only supports OS X.
 ;;
 ;;; Code:
+;;; Library Requires
+(require 'dash)
+(require 'org-ml)
 
 ;;; Customizable variables
 (defgroup org-spotify nil
-  "Store org-mode notes in a Neo4j graph database."
+  "Play spotify songs through org mode interfaces."
   :group 'org
   :prefix "org-spotify-"
   :link '(url-link :tag "Github" "https://github.com/ketan0/org-spotify"))
 
+;; Functions
 (defun org-spotify-play-href (href)
   "Plays HREF on Spotify, using the Alfred Spotify Mini Player."
   (shell-command (format "osascript -e 'tell application id \"%s\" to run trigger \"play\" in workflow \"%s\" with argument \"%s\"'"
@@ -39,17 +43,28 @@
                          "Spotify"
                          "playpause")))
 
-(defvar sample-href "spotify:artist:4r63FhuTkUYltbVAg5TQnk")
-(org-spotify-play-href sample-href)
+(defun org-spotify-set-file-href ()
+  (interactive)
+  (let ((href (read-string "Enter Spotify [track/artist/album/playlist] HREF: ")))
+    (unless (org-spotify-get-file-href)
+      (org-ml-update-section-at* (point-min)
+        (org-ml-set-children
+         (append (list (org-ml-build-keyword "SPOTIFY_HREF" href))
+                 (org-ml-get-children it))
+         it)))))
+
+(defun org-spotify-get-file-href ()
+  (-some->> (org-collect-keywords '("SPOTIFY_HREF"))
+    (car)
+    (cadr)))
 
 (defun org-spotify--maybe-play-file-href ()
+  "Hook on `find-file-hook' to potentially play music.
+Plays if in an `org-mode' file that specifies a spotify href."
   ;; TODO: compatibility if org-collect-keywords doesn't exist
   (when (eq major-mode 'org-mode)
-             (-some->> (org-collect-keywords '("SPOTIFY_HREF"))
-                  (car)
-                  (cadr)
-                  (org-spotify-play-href)
-                  )))
+    (-some->> (org-spotify-get-file-href)
+      (org-spotify-play-href))))
 
 (define-minor-mode org-spotify-mode
   "Test."
@@ -57,15 +72,9 @@
   :keymap (let ((map (make-sparse-keymap)))
             map)
   :group 'org-spotify
-
   (if org-spotify-mode
       (add-hook 'find-file-hook 'org-spotify--maybe-play-file-href)
-    (remove-hook 'find-file-hook 'org-spotify--maybe-play-file-href))
-
-  )
-
-;;
-;; (url-retrieve-synchronously )
+    (remove-hook 'find-file-hook 'org-spotify--maybe-play-file-href)))
 
 (provide 'org-spotify)
 ;;; org-spotify.el ends here
